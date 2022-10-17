1ec50b5773a50c3c67c76d6afca0cc9aadcec40b
Matthew Warman <warmanmr@gmail.com>
2022-10-17 18:06:43 -0400

    sort todo list

--- src/components/pages/TodoListPage.js
+++ src/components/pages/TodoListPage.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+import orderBy from 'lodash/orderBy';
+import classNames from 'classnames';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+
+import { useGetTodos } from '../../hooks/todos.hooks';
+
 import TodoList from '../todos/TodoList';
 
 const TodoListPage = () => {
+  const [sortBy, setSortBy] = useState('title');
+  const [sortDirection, setSortDirection] = useState('asc');
+  const { data: todos, status, isFetching } = useGetTodos();
+  const sortedTodos = orderBy(todos, [sortBy], [sortDirection]);
+
   return (
-    <div className="mt-3 h-full">
-      <TodoList />
+    <div className="h-full p-4">
+      <div className="flex items-center">
+        <div className="mr-1 text-2xl font-bold">All Todos</div>
+        {isFetching && <ArrowPathIcon className="inline-block h-6 w-6 animate-spin" />}
+      </div>
+      <div className={classNames('mt-3', { 'opacity-25': isFetching })}>
+        <TodoList
+          todos={sortedTodos}
+          isFetching={isFetching}
+          status={status}
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+          sortDirection={sortDirection}
+          setSortDirection={setSortDirection}
+        />
+      </div>
     </div>
   );
 };
--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,12 +1,20 @@
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/24/outline';
+
 import TodoListItem from './TodoListItem';
 import Placeholder from '../common/Placeholder';
 import CreateTodoButton from './CreateTodoButton';
 
-import { useGetTodos } from '../../hooks/todos.hooks';
 import { keys } from '../../utils/keys';
 
-const TodoList = () => {
-  const { data: todos, status } = useGetTodos();
+const TodoList = ({ todos, status, sortBy, setSortBy, sortDirection, setSortDirection }) => {
+  const setSort = (attr = 'title') => {
+    if (attr === sortBy) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(attr);
+      setSortDirection('asc');
+    }
+  };
 
   if (status === 'loading') {
     return (
@@ -39,10 +47,49 @@ const TodoList = () => {
   return (
     <table className="w-full">
       <thead>
-        <tr className="border-b-2 border-slate-300 font-bold">
-          <td className="p-2">Title</td>
-          <td className="p-2">Status</td>
-          <td className="p-2">Assignee</td>
+        <tr className="border-b-2 border-slate-300">
+          <td>
+            <div
+              className="flex cursor-pointer items-center p-2 hover:bg-slate-100"
+              onClick={() => setSort('title')}
+            >
+              <span className="mr-1 font-bold">Title</span>
+              {sortBy === 'title' && sortDirection === 'asc' && (
+                <ArrowUpIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+              {sortBy === 'title' && sortDirection === 'desc' && (
+                <ArrowDownIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+            </div>
+          </td>
+          <td>
+            <div
+              className="flex cursor-pointer items-center p-2 hover:bg-slate-100"
+              onClick={() => setSort('completed')}
+            >
+              <span className="mr-1 font-bold">Status</span>
+              {sortBy === 'completed' && sortDirection === 'asc' && (
+                <ArrowUpIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+              {sortBy === 'completed' && sortDirection === 'desc' && (
+                <ArrowDownIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+            </div>
+          </td>
+          <td>
+            <div
+              className="flex cursor-pointer items-center p-2 hover:bg-slate-100"
+              onClick={() => setSort('accountId')}
+            >
+              <span className="mr-1 font-bold">Assignee</span>
+              {sortBy === 'accountId' && sortDirection === 'asc' && (
+                <ArrowUpIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+              {sortBy === 'accountId' && sortDirection === 'desc' && (
+                <ArrowUpIcon className="inline-block h-4 w-4 stroke-slate-500 stroke-2" />
+              )}
+            </div>
+          </td>
         </tr>
       </thead>
       <tbody>
