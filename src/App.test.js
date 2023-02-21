74ee0ab1ab791b06deb4046ff6a186f41b6af5f8
Matthew Warman <warmanmr@gmail.com>
2023-02-21 12:53:04 -0500

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render successfully', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(getByTestId('app')).toBeDefined();
+  });
 });
