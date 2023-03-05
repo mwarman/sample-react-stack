0659abf2be9f6535a58bcec2c004d16ac6cc5792
Matthew Warman <warmanmr@gmail.com>
2023-03-05 07:03:45 -0500

    jsdoc

--- src/components/common/lists/ListColumn.js
+++ src/components/common/lists/ListColumn.js
@@ -1,9 +1,31 @@
+/**
+ * The `ListColumn` React component.
+ * @module components/common/lists/ListColumn
+ */
+
 import classNames from 'classnames';
 
 import Icon from '../icons/Icon';
 
 import { useListContext } from '../../../hooks/list.hooks';
 
+/**
+ * The `ListColumn` component renders a single column heading when displaying
+ * a list of items. This component integrates with the `ListContext` to provide
+ * sort controls.
+ * @function
+ * @param {Object} props The component properties.
+ * @param {string} [props.className] Optional additional classes applied to the column.
+ * @param {string} props.name The column display name.
+ * @param {string} props.path The item attribute path.
+ * @returns {JSXElement} JSX
+ * @example
+ *  <ListColumn
+ *    className="col-span-6 overflow-clip whitespace-nowrap"
+ *    name="Summary"
+ *    path="summary"
+ *  />
+ */
 const ListColumn = ({ className, name, path }) => {
   const { list, setSort } = useListContext();
 
--- src/components/common/lists/ListEmpty.js
+++ src/components/common/lists/ListEmpty.js
@@ -1,5 +1,23 @@
+/**
+ * The `ListEmpty` React component.
+ * @module components/common/lists/ListEmpty
+ */
+
 import Icon from '../icons/Icon';
 
+/**
+ * The `ListEmpty` component renders an empty state notification when empty,
+ * otherwise, displays the `children`.
+ * @function
+ * @param {Object} props The component properties.
+ * @param {JSXElement} props.children The content displayed when not empty.
+ * @param {boolean} [props.isEmpty=false] Indicates if the list is empty.
+ * @returns {JSXElement} JSX
+ * @example
+ * <ListEmpty isEmpty={list.length === 0}>
+ *   <div>Show when not empty</div>
+ * </ListEmpty>
+ */
 const ListEmpty = ({ children, isEmpty }) => {
   if (isEmpty) {
     return (
