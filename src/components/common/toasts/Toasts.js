847847cfdf0dc6ceab1868e8ab83ead8c7ec2997
Matthew Warman <warmanmr@gmail.com>
2023-03-07 13:56:41 -0500

    jsdoc

--- src/components/common/toasts/Toasts.js
+++ src/components/common/toasts/Toasts.js
@@ -1,6 +1,25 @@
+/**
+ * The `Toasts` React component.
+ * @module components/common/toasts/Toasts
+ */
+
 import { useToastsContext } from '../../../hooks/toasts.hooks';
 import Toast from './Toast';
 
+/**
+ * The `Toasts` component integrates with the `ToastsContext` and renders
+ * all active toast messages in the lower-left corner of the page. The `Toasts`
+ * component is typically configured once near the top of the component
+ * hierarchy.
+ * @function
+ * @returns {JSXElement} JSX
+ * @example
+ * <ToastsContextProvider>
+ *  <Header />
+ *  <Outlet />
+ *  <Toasts />
+ * </ToastsContextProvider>
+ */
 const Toasts = () => {
   const toastContext = useToastsContext();
 
