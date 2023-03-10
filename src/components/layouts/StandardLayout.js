7e66939da6ddd981ba5acf3432d6ca757adb3d16
Matthew Warman <warmanmr@gmail.com>
2023-03-10 10:55:35 -0500

    jsdoc

--- src/components/layouts/StandardLayout.js
+++ src/components/layouts/StandardLayout.js
@@ -1,3 +1,8 @@
+/**
+ * The `StandardLayout` React component.
+ * @module components/layouts/StandardLayout
+ */
+
 import { Outlet } from 'react-router-dom';
 
 import Header from '../header/Header';
@@ -5,6 +10,13 @@ import WithModals from '../common/modals/WithModals';
 import { ModalContextProvider } from '../../contexts/modal.context';
 import Toasts from '../common/toasts/Toasts';
 
+/**
+ * The `StandardLayout` component renders the standard page layout for
+ * the application. Includes support for rendering modals, top navigation,
+ * and toasts.
+ * @function
+ * @returns {JSXElement} JSX
+ */
 const StandardLayout = () => {
   return (
     <>
