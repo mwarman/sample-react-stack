d38bdfbdda48e5015b28746621eaaae1f3c64262
Matthew Warman <warmanmr@gmail.com>
2023-03-15 06:49:05 -0400

    Header component tests

---
+++ src/components/header/__tests__/Header.test.js
@@ -0,0 +1,47 @@
+import { screen, waitFor } from '@testing-library/react';
+
+import { renderWithProviders } from '../../../test/helpers';
+
+import * as authHooks from '../../../hooks/auth.hooks';
+import Header from '../Header';
+import { authenticatedUser1, notAuthenticated } from '../../../__fixtures__/authstate.fixture';
+import { account1 } from '../../../__fixtures__/accounts.fixture';
+
+const useAuthStateSpy = jest.spyOn(authHooks, 'useAuthState');
+const useGetAccountSpy = jest.spyOn(authHooks, 'useGetAccount');
+
+describe('Header', () => {
+  beforeEach(() => {
+    useAuthStateSpy.mockReturnValue({ data: notAuthenticated, isSuccess: true });
+    useGetAccountSpy.mockReturnValue({});
+  });
+
+  afterEach(() => {
+    useAuthStateSpy.mockClear();
+    useGetAccountSpy.mockClear();
+  });
+
+  it('should render successfully', () => {
+    renderWithProviders(<Header />);
+
+    expect(screen.getByTestId('header')).toBeDefined();
+  });
+
+  it('should render Sign In link to unauthenticated user', () => {
+    renderWithProviders(<Header />);
+
+    expect(screen.getByText(/Sign In/i)).toBeInTheDocument();
+  });
+
+  it('should render Sign Out link to authenticated user', () => {
+    useAuthStateSpy.mockReturnValueOnce({
+      data: authenticatedUser1,
+      isSuccess: true,
+    });
+    useGetAccountSpy.mockReturnValueOnce({ data: account1 });
+
+    renderWithProviders(<Header />);
+
+    expect(screen.getByText(/Sign Out/i)).toBeInTheDocument();
+  });
+});
