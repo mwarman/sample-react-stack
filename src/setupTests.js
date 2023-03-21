ea054a9cd7f1502b3586e6aa4ee67ccba11db7e6
Matthew Warman <warmanmr@gmail.com>
2023-03-21 10:48:42 -0400

    do not log errors during tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const originalConsoleError = global.console.error.bind(global.console);
+
+beforeAll(() => {
+  global.console.error = jest.fn();
+});
+
+afterAll(() => {
+  global.console.error = originalConsoleError;
+});
